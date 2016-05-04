0861aca00d9adaa733f89aabd2d76212b8256f4f
Ivan <wcy5211678@163.com>
2016-05-04 11:06:34 +0800

    simplify-html-loader

---
+++ index.js
@@ -0,0 +1,65 @@
+/*
+	MIT License http://www.opensource.org/licenses/mit-license.php
+	Author Tobias Koppers @sokra
+*/
+var htmlMinifier = require("html-minifier");
+var attrParse = require("./lib/attributesParser");
+var loaderUtils = require("loader-utils");
+var url = require("url");
+var assign = require("object-assign");
+var compile = require("es6-templates").compile;
+
+function randomIdent() {
+	return "xxxHTMLLINKxxx" + Math.random() + Math.random() + "xxx";
+}
+
+function getLoaderConfig(context) {
+	var query = loaderUtils.parseQuery(context.query);
+	var configKey = query.config || 'htmlLoader';
+	var config = context.options && context.options.hasOwnProperty(configKey) ? context.options[configKey] : {};
+
+	delete query.config;
+
+	return assign(query, config);
+}
+
+module.exports = function(content) {
+	this.cacheable && this.cacheable();
+	var config = getLoaderConfig(this);
+
+	if(typeof config.minimize === "boolean" ? config.minimize : this.minimize) {
+		var minimizeOptions = assign({}, config);
+
+		[
+			"removeComments",
+			"removeCommentsFromCDATA",
+			"removeCDATASectionsFromCDATA",
+			"collapseWhitespace",
+			"conservativeCollapse",
+			"removeAttributeQuotes",
+			"useShortDoctype",
+			"keepClosingSlash",
+			"minifyJS",
+			"minifyCSS",
+			"removeScriptTypeAttributes",
+			"removeStyleTypeAttributes",
+		].forEach(function(name) {
+			if(typeof minimizeOptions[name] === "undefined") {
+				minimizeOptions[name] = true;
+			}
+		});
+
+		content = htmlMinifier.minify(content, minimizeOptions);
+	}
+
+	if(config.interpolate) {
+		content = compile('`' + content + '`').code;
+	} else {
+		content = JSON.stringify(content);
+	}
+
+	return "module.exports = " + content.replace(/xxxHTMLLINKxxx[0-9\.]+xxx/g, function(match) {
+		if(!data[match]) return match;
+		return '" + require(' + JSON.stringify(loaderUtils.urlToRequest(data[match], root)) + ') + "';
+	}) + ";";
+}
